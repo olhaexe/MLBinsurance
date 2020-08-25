d89a2577b7cf68e5fe5a7027a0be87baa9449818
olhaexe <53696639+olhaexe@users.noreply.github.com>
2020-08-25 10:42:39 +0300

    Update app_prep.py

--- app_prep.py
+++ app_prep.py
@@ -82,5 +82,5 @@ def exceptions(e):
 
 
 if __name__ == '__main__':
-    #app.run(debug=True)
-    serve(app, host='0.0.0.0', port=80)
+    app.run(debug=True)
+    #serve(app, host='0.0.0.0', port=80)
